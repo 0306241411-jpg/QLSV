9af62cf65cb9a7cfc75ed40fff5981d7fb5e271c
Võ Ngọc Tiên <0306241411@caothang.edu.vn>
2026-06-29 14:10:23 +0700

    Code So Yeu Ly Lich

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sơ Yếu Lý Lịch</title>
+</head>
+<body>
+    <h1>Trường Cao Đẳng Kĩ Thuật Cao Thắng</h1>
+    <p>CD CNTT 24WED</p>
+    <p>Võ Ngọc Tiên</p>
+    <p>0306241411</p>
+</body>
+</html>
