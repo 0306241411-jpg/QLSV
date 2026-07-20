eb77a1be5168a50c5f1281ab6f88935fb9573b52
0306241411-jpg <0306241411@caothang.edu.vn>
2026-07-20 14:25:29 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', function() {
+    return redirect('login');
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
